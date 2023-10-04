dfc5598d99ef578d50c72e1f76f275f16435922d
Anastasia <ana.kuk@gmx.net>
2023-10-04 16:22:43 +0200

    Implement console output color and create test dir

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+let color = randomColor();
 
 function pattern() {
   for (let i = 1; i < 4; i++) {
-    console.log('%c##### ##### ########### ##### #####', 'color: #334455');
+    console.log(chalk.hex(color)('###############################'));
   }
-  console.log('##### #####' + '             ' + '##### #####');
-  console.log('##### #####   ' + color + '   ##### #####');
-  console.log('##### #####' + '             ' + '##### #####');
+  console.log(chalk.hex(color)('##########' + '           ' + '##########'));
+  console.log(chalk.hex(color)('##########  ' + color + '  ##########'));
+  console.log(chalk.hex(color)('##########' + '           ' + '##########'));
   for (let i = 1; i < 4; i++) {
-    console.log('##### ##### ########### ##### #####');
+    console.log(chalk.hex(color)('###############################'));
   }
 }
 
-let letters = '0123456789ABCDEF';
-let color = '#';
-for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
-
 if (argv.length === 2) {
-  // This happens if there is no user input
+  //node index.js
+  pattern();
+} else if (argv.length === 3) {
+  //node index.js hue
+  //console.log('You chose ' + argv[2]);
+  color = randomColor({
+    luminosity: 'random',
+    hue: argv[2],
+  });
   pattern();
 } else {
-  console.log('You chose ' + argv[2]);
-  // color = argv[2];
-  // pattern();
+  //node index.js hue luminosity
+  //console.log('You chose: ' + argv[3] + ' ' + argv[2]);
+  color = randomColor({
+    luminosity: argv[3],
+    hue: argv[2],
+  });
+  pattern();
 }
